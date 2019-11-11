d3b1934b4d5a818b9ad7376da89bb10163d82fa3
taotie-lab <taotiefll@163.com>
2019-11-11 20:30:12 +0800

    wlh

---
+++ wlh/js/banner2ajax.js
@@ -0,0 +1,47 @@
+
+$(function () {
+    $.ajax({
+        url: './w_banner.json',
+        type: 'get',
+        dataType: 'json',
+        cache: false,
+        success: function (json) {
+            var resultlis2 = '';
+            var resultdls2 = '';
+            var json2 =json.banner2.imgurl;           
+            $.each(json2, function (index, item) {
+                if (index <= 1) {
+                    resultlis2 +=
+                        `
+                        <li>
+                            <a href="">
+                                 <img src="${json2[index]}" alt="">
+                            </a>
+                        </li> 
+                        `
+                } else {
+                    resultdls2 +=
+                        `
+                        <dl>
+                            <a href="">
+                                 <img src="${json2[index]}" alt="">
+                            </a>
+                        </dl> 
+                        `
+                }
+            });
+            $('.w_lisbanner2').html(resultlis2);
+            $('.w_lis2banner2').html(resultdls2);
+        }
+    });
+})
+
+
+
+
+
+
+
+
+
+
